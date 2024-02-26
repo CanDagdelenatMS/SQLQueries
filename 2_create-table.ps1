4859346ebdb092f2c31e098ba6445b16510c97c8
sytahmtcbn <100773524+sytahmtcbn@users.noreply.github.com>
2024-02-26 15:24:33 +0300

    Add files via upload
    New versions added.

---
+++ 2_create-table.ps1
@@ -0,0 +1,49 @@
+﻿$createquery= @”
+   IF EXISTS (SELECT 1 FROM SYS.objects WHERE name ='Findings')
+   DROP TABLE dbo.Findings
+
+    CREATE TABLE [dbo].[Findings](
+	[ComputerName] [varchar](500) NULL,
+	[CPUName] [varchar](500) NULL,
+	[NumberOfCores] [varchar](500) NULL,
+	[NumberOfEnabledCore] [varchar](500) NULL,
+	[NumberOfLogicalProcessors] [varchar](500) NULL,
+	[NoOfSockets] [varchar](100) NULL,
+	[MemorySize] [varchar](500) NULL,
+	[DiskTotalSize] [varchar](500) NULL,
+	[DiskSizeRemaining] [varchar](500) NULL,
+	[Language] [varchar](500) NULL,
+	[TimeZone] [varchar](500) NULL,
+	[Manufacturer] [varchar](500) NULL,
+	[SystemFamily] [varchar](500) NULL,
+	[SystemSKUNumber] [varchar](500) NULL,
+	[Last3InstalledUpdates] [varchar](500) NULL,
+	[MountPoints] [varchar](1000) NULL,
+	[MountPointTotalSize] [varchar](1000) NULL,
+	[MountPointSizeRemaining] [varchar](1000) NULL,
+	[SQLServer_SrvAccount] [varchar](500) NULL,
+	[SQLAgent_SrvAccount] [varchar](500) NULL,
+	[ClusterName] [varchar](500) NULL,
+	[QuorumResource] [varchar](500) NULL,
+	[QuorumType] [varchar](500) NULL,
+	[SQLClusterName] [varchar](500) NULL,
+	[SQLClusterOwnerNode] [varchar](500) NULL,
+	[SQL_InstanceName] [varchar](500) NULL,
+	[SQLAvailabilityGroupName] [varchar](500) NULL,
+	[SQLAvailabilityGroupOwner] [varchar](500) NULL,
+	[NetworkName] [varchar](500) NULL,
+	[NetworkNameOwner] [varchar](500) NULL,
+	[DAGDBNames] [nvarchar](max) NULL,
+	[DAGDBReplicas] [varchar](500) NULL,
+	[ODBCSources] [varchar](500) NULL,
+    [LocalAdminUsers] [varchar](500) NULL,
+	[LogDate] [smalldatetime] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+ALTER TABLE [dbo].[Findings] ADD  CONSTRAINT [DF__Findings__LogDat__098BD816]  DEFAULT (getdate()) FOR [LogDate]
+GO
+"@
+
+$dbname= "Zperf"
+$servername="DBAMON01"
+Invoke-Sqlcmd -ServerInstance $servername -Database $dbname -Query $createquery
---
+++ 3_InsertTable.ps1
@@ -0,0 +1,18 @@
+﻿$findings= Import-Csv .\CompInfo.csv
+foreach ($finding in $findings) {
+$InsertQuery= @"
+INSERT INTO Findings (ComputerName, CPUName, NumberOfCores, NumberOfEnabledCore, NumberOfLogicalProcessors,NoOfSockets, MemorySize, DiskTotalSize, DiskSizeRemaining, Language, 
+TimeZone, Manufacturer, SystemFamily, SystemSKUNumber, Last3InstalledUpdates, MountPoints, MountPointTotalSize, MountPointSizeRemaining, SQLServer_SrvAccount, 
+SQLAgent_SrvAccount, ClusterName, QuorumResource, QuorumType, SQLClusterName,SQLClusterOwnerNode, SQL_InstanceName, SQLAvailabilityGroupName, SQLAvailabilityGroupOwner, NetworkName, NetworkNameOwner, 
+DAGDBNames, DAGDBReplicas, ODBCSources,LocalAdminUsers)
+VALUES (`'$($finding.ComputerName)`', `'$($finding.CPUName)`', `'$($finding.NumberOfCores)`', `'$($finding.NumberOfEnabledCore)`', `'$($finding.NumberOfLogicalProcessors)`',
+  `'$($finding.NoOfSockets)`',`'$($finding.MemorySize)`', `'$($finding.DiskTotalSize)`', `'$($finding.DiskSizeRemaining)`', `'$($finding.Language)`', `'$($finding.TimeZone)`',
+   `'$($finding.Manufacturer)`', `'$($finding.SystemFamily)`', `'$($finding.SystemSKUNumber)`', `'$($finding.Last3InstalledUpdates)`', `'$($finding.MountPoints)`',
+    `'$($finding.MountPointTotalSize)`', `'$($finding.MountPointSizeRemaining)`', `'$($finding.SQLServer_SrvAccount)`', `'$($finding.SQLAgent_SrvAccount)`',
+     `'$($finding.ClusterName)`', `'$($finding.QuorumResource)`', `'$($finding.QuorumType)`',`'$($finding.SQLFailoverClusterName)`',`'$($finding.SQLOwnerNode)`',
+      `'$($finding.SQL_InstanceName)`', `'$($finding.SQLAvailabilityGroupName)`',
+      `'$($finding.SQLAvailabilityGroupOwner)`', `'$($finding.NetworkName)`', `'$($finding.NetworkNameOwner)`', `'$($finding.DAGDBNames)`', `'$($finding.DAGDBReplicas)`',
+      `'$($finding.ODBCSources)`',  `'$($finding.LocalAdminUsers)`')
+"@
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database zPERF -Query $InsertQuery
+}
